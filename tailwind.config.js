4d354afbee60a82c98a4eb433786a811abd32c6a
Muhammad Azri <lenovorosak@gmail.com>
2021-08-30 22:52:15 +0800

    update the logic and the color

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,16 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        vibrantBlue: {
+          DEFAULT: '#17a2b8'
+        },
+        borderBlue: {
+          DEFAULT: '#06BDC1'
+        }
+      }
+    },
   },
   variants: {
     extend: {},
